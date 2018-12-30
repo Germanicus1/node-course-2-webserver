6a3e4aaf21583f473b229e4bf5f9b051ae0b7a94
Peter Kerschbaumer <peter@kerschbaumer.se>
2018-12-30 15:46:21 +0100

    Added projects portfolio page and changed start script to use npmmon

--- server.js
+++ server.js
@@ -43,9 +43,15 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     })
 })
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    })
+})
+
 
 
 app.listen(port, () => {
